4245254beed754370a4c3770b67c2d9a297323bd
leo389 <40183643+leo389@users.noreply.github.com>
2020-02-11 16:09:20 +0000

    Add files via upload
    Bug fixes:
    URL now set based on Hub serial digit
    Outputs JSON format

--- myenergi_login.py
+++ myenergi_login.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 #
 #   Python Project to collect data from MyEnergi Server
 #
@@ -12,28 +11,25 @@ import time
 from configparser import SafeConfigParser
 import logging
 import sys
-#import pprint
-#import paho.mqtt.client as mqtt
 import schedule
 from datetime import datetime
 import os
 import json
 import requests
-#from requests.auth import HTTPDigestAuth
-import urllib.request
-import socket
+from requests.auth import HTTPDigestAuth
+from pprint import pprint
 
 config_file = 'config.ini'
 
 ###########
 ## Various URL's..
+## For more see https://github.com/twonk/MyEnergi-App-Api
+# Note: Modify Character  X to reflect last digit in Hub serial number
 
-zappi_base_url = 'https://s9.myenergi.net/'
-zappi_status_url = 'https://s9.myenergi.net/cgi-jstatus-Z'
+zappi_base_url = 'https://sX.myenergi.net/'
+zappi_status_url = 'https://sX.myenergi.net/cgi-jstatus-Z'
 
 ###########
-class DataTimeout(Exception):
-    pass
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.info("Startup myenergi_login: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
@@ -43,7 +39,6 @@ config_file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), conf
 parser = SafeConfigParser()
 if os.path.exists(config_file_path):
     logging.info("Loaded config file " + config_file_path)
-    #candidates = [ 'config.ini', 'my_config.ini' ]
     candidates = config_file_path
     found = parser.read(candidates)
     hub_serial = parser.get('get-myenergi-info', 'hub_serial')
@@ -55,49 +50,33 @@ else:
     logging.error("ERROR: Config file not found " + config_file_path)
     quit()
 
+# Set server address based on last digit in Hub serial number
+zappi_base_url = zappi_base_url[:9] + hub_serial[-1:] + zappi_base_url[10:]
+zappi_status_url = zappi_status_url[:9] + hub_serial[-1:] + zappi_status_url[10:]
+
 # Get data from server
 def get_zappi_status():
     logging.info("Geting Zappi info from Hub..")
-    logging.info("login = %s , password = %s" % (hub_serial,hub_password) )
-    logging.info("Prepare Session")
+    logging.debug("login = %s , password = %s" % (hub_serial,hub_password))
+    logging.info("Based on Hub serial number using base URL: " + zappi_base_url)
 
-    url = 'http://s9.myenergi.net/cgi-jstatus-Z'
-
-    req = urllib.request.Request(url)
-
-    req.add_header('User-Agent', 'Wget/1.14 (linux-gnu)')
-
-    auth_handler = urllib.request.HTTPPasswordMgr()
-    auth_handler.add_password(user=hub_serial,
-                  uri=url,
-                  realm='MyEnergi Telemetry',
-                  passwd=hub_password)
-
-    handler = urllib.request.HTTPDigestAuthHandler(auth_handler)
-    opener = urllib.request.build_opener(handler)
-    urllib.request.install_opener(opener)
-
-    try:
-        stream = urllib.request.urlopen(req, timeout=20)
-        logging.info('Response was %s', stream.getcode())
-    except urllib.error.HTTPError:
-        raise DataTimeout
-    except urllib.error.URLError:
-        raise DataTimeout
-    except socket.timeout:
-        raise DataTimeout
-    except http.client.RemoteDisconnected:
-        raise DataTimeout
-    except ConnectionResetError:
-        raise DataTimeout
-    try:
-        raw_data = stream.read()
-        return json.loads(raw_data)
-    except socket.timeout:
-        raise DataTimeout
+    h = {'User-Agent': 'Wget/1.14 (linux-gnu)'}
+    
+    r = requests.get(zappi_status_url, headers = h, auth=HTTPDigestAuth(hub_serial, hub_password), timeout=10)
+    if (r.status_code == 200):
+        logging.info("Login successful..") 
+    elif (r.status_code == 401):
+        logging.info("Login unsuccessful!!! Please check username, password or URL")
+        quit()
+    else:
+        logging.info("login unsuccessful, returned code: " + r.status_code)
+        quit()
+    
+    logging.info(r.headers) 
+    pprint(r.json())
 
     logging.info("End update time: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    logging.info("Schedule API update every " + GET_UPDATE_INTERVAL + "min")
+    
     return ()
 
 #########################################################################################################################
